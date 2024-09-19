678c1187b5ea9cf29242507340f858db19310e97
roomey_rahman <roomeyrahman@gmail.com>
2024-09-19 21:58:30 +0600

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Module, ClassSerializerInterceptor } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './common/filters';
+import {
+  LoggingInterceptor,
+  TransformInterceptor,
+} from './common/interceptors';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfigService } from './common/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dbConfigService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
